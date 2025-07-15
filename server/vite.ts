d8df18e20cfaefed07501efbc1d5d94d8dcc7d1c
LorenzoAlgeri <148132548+LorenzoAlgeri@users.noreply.github.com>
2025-07-15 21:20:53 +0000

    update

--- server/vite.ts
+++ server/vite.ts
@@ -76,10 +76,10 @@ export function serveStatic(app: Express) {
     );
   }
 
-  app.use(express.static(distPath));
+  // app.use(express.static(distPath));
 
   // fall through to index.html if the file doesn't exist
-  app.use("*", (_req, res) => {
-    res.sendFile(path.resolve(distPath, "index.html"));
-  });
+  // app.use("*", (_req, res) => {
+    //res.sendFile(path.resolve(distPath, "index.html"));
+  // });
 }
